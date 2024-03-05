ebd2dce55f7becb7528bfe0b7a9ec2de9c508dc9
moonyoungseong <ansdudtjd123@naver.com>
2024-03-05 21:56:33 +0900

    콜라이더 적용
    wheel collider 적용

--- CarRaceGame/Assets/Scripts/CarController.cs
+++ CarRaceGame/Assets/Scripts/CarController.cs
@@ -4,27 +4,13 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public Rigidbody[] wheels = new Rigidbody[4]; // Rigidbody 배열로 변경
-    public float torque = 200;  // 회전힘
-
     void Start()
     {
-        // Rigidbody 컴포넌트를 찾아 배열에 할당
-        for (int i = 0; i < wheels.Length; i++)
-        {
-            wheels[i] = transform.GetChild(i).GetComponent<Rigidbody>();
-        }
+ 
     }
 
-    private void FixedUpdate()
+   void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            for (int i = 0; i < wheels.Length; i++)
-            {
-                // Rigidbody에 토크를 적용
-                wheels[i].AddRelativeTorque(Vector3.forward * torque);
-            }
-        }
+
     }
 }
