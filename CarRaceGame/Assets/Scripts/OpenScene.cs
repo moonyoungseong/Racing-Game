b77390ca5237887efbf2d4ede196dfbd4a525784
moonyoungseong <ansdudtjd123@naver.com>
2024-03-10 14:04:42 +0900

    씬 로드 테스트
    비동기식 방식
    로드 중 대기

---
+++ CarRaceGame/Assets/Scripts/OpenScene.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class OpenScene : MonoBehaviour
+{
+    // 비동기 방식으로 다른 씬을 로드하는 코루틴
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // 씬 로드가 완료될 때까지 대기
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    // 예시: 다른 씬을 로드하는 버튼 클릭 이벤트 처리 메서드
+    public void OnTutorialButtonClick()
+    {
+        StartCoroutine(LoadSceneAsync("Tutorial_scene"));
+    }
+}
